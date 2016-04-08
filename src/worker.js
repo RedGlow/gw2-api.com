1637528fd17b39cabb36faa994fa2f563f3bd28a
Mattia Belletti <mattia.belletti@gmail.com>
2016-04-08 06:37:24 -0400

    Basic configuration for starting workers.

--- src/worker.js
+++ src/worker.js
@@ -1,14 +1,31 @@
 require('babel-polyfill')
+const config = require('configure')
+const logger = require('./helpers/logger.js')
 const mongo = require('./helpers/mongo.js')
 const itemWorker = require('./workers/item.js')
 const gemWorker = require('./workers/gem.js')
 const skinWorker = require('./workers/skin.js')
 const recipeWorker = require('./workers/recipe.js')
 
+const workers = [
+  { condition: config.startItemWorker,
+    method: itemWorker.initialize },
+  { condition: config.startGemWorker,
+    method: itemWorker.initialize },
+  { condition: config.startSkinWorker,
+    method: itemWorker.initialize },
+  { condition: config.startRecipeWorker,
+    method: itemWorker.initialize }
+]
+
 // Connect to the DB and get working! :)
 mongo.connect().then(() => {
-  itemWorker.initialize()
-  gemWorker.initialize()
-  skinWorker.initialize()
-  recipeWorker.initialize()
+  let promises = workers
+    .filter(worker => worker.condition)
+    .map(worker => worker.method())
+  Promise
+    .all(promises)
+    .catch(e => {
+      logger.error(e);
+    })
 })
